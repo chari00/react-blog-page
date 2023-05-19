c5a5544c543cde6155068f2d1c77fb6e5f9ceb11
chari00 <macharitocruzuk@gmail.com>
2023-05-19 21:24:50 +0100

    create ArticleList js file to store the reusable codes for displaying the articles.

---
+++ src/components/ArticlesList.js
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+
+export default function ArticlesList ({articles}) {
+    return (
+        //link article content to display on the page 
+        //loop over each article content.
+        <>
+        {articles.map(article => 
+            <Link key={article.name} to={`/articles/${article.name}`}>
+                <h1>{article.title}</h1>
+                <p >{article.content[0].substring(0, 150)}...</p>
+            </Link>)}
+        </>
+    )
+}
--- src/pages/ArticleListPage.js
+++ src/pages/ArticleListPage.js
@@ -1,16 +1,10 @@
+import ArticlesList from "../components/ArticlesList"
 import articles from "./articleContent"
-import { Link } from "react-router-dom"
 
 export default function ArticleListPage () {
     return (
-        //link article content to display on the page 
-        //loop over each article content.
         <>
-        {articles.map(article => 
-        <Link key={article.name} to={`/articles/${article.name}`}>
-            <h1>{article.title}</h1>
-             <p >{article.content[0].substring(0, 150)}...</p>
-        </Link>)}
+        <ArticlesList articles={articles}/>
         
         </>
     )
