9955554b4542bee55f104c052014206334c6e083
chari00 <macharitocruzuk@gmail.com>
2023-05-19 13:38:07 +0100

    link article content to display on the page.

--- src/pages/ArticleListPage.js
+++ src/pages/ArticleListPage.js
@@ -1,7 +1,17 @@
+import articles from "./articleContent"
+import { Link } from "react-router-dom"
+
 export default function ArticleListPage () {
     return (
+        //link article content to display on the page 
+        //loop over each article content.
         <>
-        <h1>this is the Article list page</h1>
+        {articles.map(article => 
+        <Link key={article.name} to={`/articles/${article.name}`}>
+            <h1>{article.title}</h1>
+             <p >{article.content[0].substring(0, 150)}...</p>
+        </Link>)}
+        
         </>
     )
 };
