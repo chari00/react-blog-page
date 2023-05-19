a7d86cd0ea93f9a243c9c38a6165a44a927f5001
chari00 <macharitocruzuk@gmail.com>
2023-05-19 22:15:31 +0100

    add contents to about page and home page.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,26 @@
-
-export default function HomePage () {
-    return (
-        <>
-        <h1>this is the home page</h1>
-        </>
-    )
-};
+export default function HomePage() {
+  return (
+    <>
+      <h1>this is the home page</h1>
+      <p>
+        soluta voluptates officiis quod, autem quae fuga commodi nisi tempora.
+        Optio, exercitationem obcaecati harum aspernatur alias, maxime facere
+        non architecto placeat recusandae dignissimos? Modi voluptas veniam
+        nihil sapiente beatae. Nobis, earum saepe voluptatem libero amet placeat
+        id dicta reiciendis! Molestias nihil amet odit, debitis quis,
+        exercitationem magnam consectetur voluptatum culpa, ullam qui explicabo
+        maiores laborum dicta animi recusandae quisquam? Dolorga? Officia nihil
+        nesciunt itaque eligendi inventore odit in illum eaque quam, ut harum
+        quas aliquid delectus reiciendis, repudiandae accusantium rem? Ab quas
+        Corporis iure adipisci soluta accusantium pariatur doloribus aut iste
+        facilis, nam molestiae temporibus recusandae, quidem sed molestias porro
+        sint voluptates fuga, sequi dolor. Veritatis atque excepturi eveniet
+        odio aliquid fugit! Itaque, eius aperiam quibusdam sed delectus autem
+        quod repellat architecto? Totam vitae unde cum laborum veritatis? Velit
+        dicta perferendis adipisci, exercitationem quia incidunt voluptates
+        iusto veniam mollitia? Autem, voluptatum unde. Qique, modi architecto
+        iure placeat, aut, unde doloremque repella
+      </p>
+    </>
+  );
+}
