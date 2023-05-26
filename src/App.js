c3f15522047fa1e3bea45c088f0b6e1afe92d333
chari00 <macharitocruzuk@gmail.com>
2023-05-26 18:55:23 +0100

    update imports and route in app.js file.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import ArticlePage from './pages/ArticlePage';
 import HomePage from './pages/HomePage';
 import NavBar from './NavBar';
 import PageNotFound from './pages/PageNotFound';
+import LogInPage from './pages/LogInPage';
+import CreateAcctPage from './pages/CreateAcctPage';
 
 function App() {
   return (
@@ -21,6 +23,9 @@ function App() {
       <Route path='/about' element= {<AboutPage/>}/>
       <Route path='/articles' element={<ArticleListPage/>}/>
       <Route path='/articles/:articleId' element={<ArticlePage/>}/>
+      <Route path='/login' element={<LogInPage/>}/>
+      <Route path='/CreateAcct' element={<CreateAcctPage/>}/>
+
 
       {/*add route with path '*' to display page not found on all route except from the ones defined above.  */}
       <Route path='*' element={<PageNotFound/>}/>
