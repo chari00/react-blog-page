ee64a4f670d75818f065353288c891968df1057d
chari00 <macharitocruzuk@gmail.com>
2023-05-26 18:15:31 +0100

    import axios and use hooks useEffect to get the response date from the article info, and set state to update upvotes and comments. Add components Comment form and Comment list

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,12 +1,36 @@
 import { useParams } from "react-router-dom";
 import articles from './articleContent';
 import PageNotFound from './PageNotFound'
+import { useState, useEffect } from "react";
+import axios from 'axios';
+import CommentsList from "../components/CommentsList";
+import AddCommentForm from "../components/AddCommentForm";
 
 
 export default function ArticlePage () {
+    const [articleInfo, setArticleInfo] = useState({upvotes:0, Comments:[]});
+     const {articleId} =useParams();
+
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+
+            const response = await axios.get(`/api/articles/${articleId}`)
+            const newArticleInfo = response.data;
+            setArticleInfo(newArticleInfo);
+        }
+        loadArticleInfo();
+
+        },[]);
+
     //use the url parameter value to load the correct article from article content file, display on the page.
-    const {articleId} =useParams();
+   
     const article = articles.find(article => article.name === articleId);
+    const addUpvote =async ()=> {
+        const response =await axios.put(`/api/articles/${articleId}/upvote`);
+        const updatedArticle = response.data;
+        setArticleInfo(updatedArticle);
+
+    }
 
     if (!article) {
         return <PageNotFound/>
@@ -15,7 +39,11 @@ export default function ArticlePage () {
     return (
         <>
         <h3>{article.title}</h3>
-        {article.content.map(paragraph => (<p key ={paragraph}>{paragraph}</p>))}
+        <button onClick={addUpvote}>upvote</button>
+        <p> This article has {articleInfo.upvotes} upvotes</p>
+        {article.content.map((paragraph, i)=> (<p key ={i}>{paragraph}</p>))}
+        <AddCommentForm articleName={articleId} onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)}/>
+        <CommentsList comments={articleInfo.comments}/>
         </>
-    )
+    );
 };
