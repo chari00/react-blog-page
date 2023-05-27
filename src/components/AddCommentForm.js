38a494db629779967c8b2eeb3cea340818164dbd
chari00 <macharitocruzuk@gmail.com>
2023-05-27 14:35:00 +0100

    add auth token and remove the label for user name input and replace  with a conditional statement to display the user email address.

--- src/components/AddCommentForm.js
+++ src/components/AddCommentForm.js
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import axios from "axios";
+import useUser from "../hooks/useUser";
 
 export default function AddCommentForm({articleName, onArticleUpdated}) {
     const [name, setName] =useState('');
     const [commentText, setCommentText] = useState('');
+    const {user} = useUser;
+
+
     const addComment = async () => {
+        const token =user && await user.getIdToken();
+        const headers = token ? {authtoken: token} : {};
         const response = await axios.post(`/api/articles/${articleName}/comment`,{postedBy: name,
-        text: commentText});
+        text: commentText}, {headers});
         const updatedArticle = response.data;
         onArticleUpdated(updatedArticle);
         setName('');
@@ -16,8 +22,9 @@ export default function AddCommentForm({articleName, onArticleUpdated}) {
         <>
         <div id='add-comment-form'>
             <h3> Add a comment</h3>
-            <label> Name: <input value={name} type="text" onChange={e =>setName(e.target.value)}/></label>
-            <label>Comment: <textarea value={commentText} onChange={e => setCommentText(e.target.value)}/></label>
+            {user && <p>you are posting as {user.email}</p>}
+            {/* <label> Name: <input value={name} type="text" onChange={e =>setName(e.target.value)}/></label> */}
+            <textarea value={commentText} onChange={e => setCommentText(e.target.value)}/>
             <button onClick={addComment}> Add Comment</button>
 
         </div>
